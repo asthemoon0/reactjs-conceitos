df6625d4adb01b98de63f91137bc6ded4acea340
asthemoon0 <47937104+asthe0@users.noreply.github.com>
2020-06-17 18:29:13 +0900

    added features

--- src/App.js
+++ src/App.js
@@ -1,31 +1,48 @@
-import React from "react";
+import React, { useState, useEffect } from 'react'
+import api from './services/api'
 
-import "./styles.css";
+import './styles.css'
 
 function App() {
+  const [repositories, setRepository] = useState([])
+
+  useEffect(() => {
+    api.get('repositories').then(response => [setRepository(response.data)])
+  }, [])
+
   async function handleAddRepository() {
-    // TODO
+    const response = await api.post('repositories', {
+      title: `Desafio ReactJS ${Date.now()}`,
+      url:
+        'https://github.com/Rocketseat/bootcamp-gostack-desafios/tree/master/desafio-conceitos-nodejs',
+      techs: ['Node.js', 'React'],
+    })
+
+    setRepository([...repositories, response.data])
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    await api.delete(`repositories/${id}`)
+
+    const filteredArray = repositories.filter(item => item.id !== id) //Return everything but the deleted item.
+
+    setRepository(filteredArray)
   }
 
   return (
     <div>
-      <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+      <ul data-testid='repository-list'>
+        {repositories.map(repo => (
+          <li key={repo.id}>
+            {repo.title}
+            <button onClick={() => handleRemoveRepository(repo.id)}>Remover</button>
+          </li>
+        ))}
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
